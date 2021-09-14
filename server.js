ddce6cf1fd7f9581321d6acb22ef94b5cc2f82f8
kirafaye99 <kirademarco99@gmail.com>
2021-09-14 07:59:38 -0700

    handlebars

---
+++ server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const tailwindcss = require('tailwindcss');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
