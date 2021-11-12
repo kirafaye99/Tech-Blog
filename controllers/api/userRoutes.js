64fb549620ecbb54526373533644f3800d70b7cd
kirafaye99 <kirademarco99@gmail.com>
2021-11-11 17:09:11 -0800

    trying to fix homepage and login

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,20 +2,47 @@ const router = require('express').Router();
 const bcrypt = require('bcrypt');
 const User = require('../../models/User');
 
-router.post('/', async (req, res) => {
-    try {
-      const userData = await User.create(req.body);
-  
+router.get('/', (req, res) => {
+  User.findAll({
+  })
+    .then(userData => res.json(userData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  })
+    .then(userData => {
       req.session.save(() => {
         req.session.user_id = userData.id;
-        req.session.logged_in = true;
+        req.session.username = userData.username;
+        req.session.loggedIn = true;
   
-        res.status(200).json(userData);
+        res.json(userData);
       });
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
+    })
+});
+
+// router.post('/', async (req, res) => {
+//     try {
+//       const userData = await User.create(req.body);
+  
+//       req.session.save(() => {
+//         req.session.user_id = userData.id;
+//         req.session.logged_in = true;
+  
+//         res.status(200).json(userData);
+//       });
+//     } catch (err) {
+//       res.status(400).json(err);
+//     }
+//   });
 
 // Added comments describing the functionality of this `login` route
 router.post('/login', async (req, res) => {
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,19 +1,44 @@
 const router = require('express').Router();
-const { Post } = require('../models');
+const { Post, User} = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
+// router.get('/', async (req, res) => {
+//   try {
+//     const posts =  await Post.findALL()
+//     res.render('homepage', {
+//       posts,
+//       logged_in: req.session.logged_in
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+router.get('/', async (req, res) => {
   try {
-    const posts =  await Post.findALL({})
-    res.render('homepage', {
-      posts,
-      logged_in: req.session.logged_in
-    });
+    res.render('homepage', {});
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/login')
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('signup');
+});
+
 
 module.exports = router;
