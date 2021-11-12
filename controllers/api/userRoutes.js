f4bc6ab63d3ca96d0bac69e09a916475fe9244e1
kirafaye99 <kirademarco99@gmail.com>
2021-11-11 17:48:15 -0800

    still trying to fix login

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 const User = require('../../models/User');
+const auth = require('../../utils/auth')
 
 router.get('/', (req, res) => {
   User.findAll({
@@ -29,46 +30,105 @@ router.post('/', (req, res) => {
     })
 });
 
-// router.post('/', async (req, res) => {
-//     try {
-//       const userData = await User.create(req.body);
-  
-//       req.session.save(() => {
-//         req.session.user_id = userData.id;
-//         req.session.logged_in = true;
-  
-//         res.status(200).json(userData);
-//       });
-//     } catch (err) {
-//       res.status(400).json(err);
-//     }
-//   });
+// router.get('/:id', (req, res) => {
+//   User.findOne({
+//     where: {
+//       id: req.params.id,
+//     },
+//     include: [
+//       {
+//         model: Post,
+//         attributes: ['id', 'title', 'post_content', 'created_at'],
+//       },
+//       {
+//         model: Comment,
+//         attributes: ['id', 'comment_text', 'created_at'],
+//         include: {
+//           model: Post,
+//           attributes: ['title'],
+//         },
+//       },
+//     ],
+//   })
+//     .then((userData) => {
+//       if (!userData) {
+//         res.status(404).json({ message: 'No user found with this id' });
+//         return;
+//       }
+//       res.json(userData);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
 
-// Added comments describing the functionality of this `login` route
-router.post('/login', async (req, res) => {
-  try {
-    // we search the DB for a user with the provided email
-    const userData = await User.findOne({ where: { email: req.body.email } });
+// router.put('/:id', withAuth, (req, res) => {
+//   User.update(req.body, {
+//     individualHooks: true,
+//     where: {
+//       id: req.params.id,
+//     },
+//   })
+//     .then((userData) => {
+//       if (!userData) {
+//         res.status(404).json({ message: 'No user found with this id' });
+//         return;
+//       }
+//       res.json(userData);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
+
+// router.delete('/:id', withAuth, (req, res) => {
+//   User.destroy({
+//     where: {
+//       id: req.params.id,
+//     },
+//   })
+//     .then((userData) => {
+//       if (!userData) {
+//         res.status(404).json({ message: 'No user found with this id' });
+//         return;
+//       }
+//       res.json(userData);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
+
+
+router.post('/login', (req, res) => {
+  User.findOne({
+    where: {
+      email: req.body.email,
+    },
+  }).then((userData) => {
     if (!userData) {
-      // the error message shouldn't specify if the login failed because of wrong email or password
-      res.status(404).json({ message: 'Login failed. Please try again!' });
+      res.status(400).json({ message: 'No user with that email address!' });
       return;
     }
-    // use `bcrypt.compare()` to compare the provided password and the hashed password
-    const validPassword = await bcrypt.compare(
-      req.body.password,
-      userData.password
-    );
-    // if they do not match, return error message
+
+    const validPassword = userData.checkPassword(req.body.password);
+
     if (!validPassword) {
-      res.status(400).json({ message: 'Login failed. Please try again!' });
+      res.status(400).json({ message: 'Incorrect password!' });
       return;
     }
-    // if they do match, return success message
-    res.status(200).json({ message: 'You are now logged in!' });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.username = userData.username;
+      req.session.loggedIn = true;
+
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+  });
 });
 
 router.post('/logout', (req, res) => {
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,18 +2,6 @@ const router = require('express').Router();
 const { Post, User} = require('../models');
 const withAuth = require('../utils/auth');
 
-// router.get('/', async (req, res) => {
-//   try {
-//     const posts =  await Post.findALL()
-//     res.render('homepage', {
-//       posts,
-//       logged_in: req.session.logged_in
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
 router.get('/', async (req, res) => {
   try {
     res.render('homepage', {});
